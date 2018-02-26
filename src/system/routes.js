4e495d8d6c9678ef515853bd5a9dfe467aa19774
YuriyPimenov <yuripimenov1991@gmail.com>
2018-02-26 16:15:44 +0500

    Создали и добавили меню в App.vue

--- src/system/routes.js
+++ src/system/routes.js
@@ -1,7 +1,15 @@
 import index from '../pages/index.vue'
 import vuejs from '../pages/vuejs.vue'
+import auth from '../pages/auth.vue'
+import linux from '../pages/linux.vue'
+import cpp from '../pages/cpp.vue'
+import js from '../pages/js.vue'
 
 export const routes = [
 	{ path:'/', 	 component:index },
-	{ path:'/vuejs', component:vuejs }
+	{ path:'/vuejs', component:vuejs },
+	{ path:'/auth',  component:auth },
+	{ path:'/linux', component:linux },
+	{ path:'/cpp', 	 component:cpp },
+	{ path:'/js',    component:js }
 ]
