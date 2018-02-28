c903679c1c4218f076a2435f4fa1eec1285c5acf
YuriyPimenov <yuripimenov1991@gmail.com>
2018-02-28 15:51:26 +0500

    404 animation

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,10 +1,38 @@
 <template>
-  <div class="error404">
-    <p>ERROR 404!!!!</p>   
+  <div class="pageNotFound">
+    <img src="../assets/img/logo.png">
+    <div class="output">
+      <h3>404 - три раза</h3>
+      <div class="Ku" @click="kuShowTwo=true">
+        <a>Ку</a>
+      </div>
+
+      <transition name="ku-animation">
+          <div class="Ku" v-show="kuShowTwo" @click="kuShowLast=true">
+            <a>Ку</a>
+          </div>
+      </transition>
+      <transition name="ku-animation">
+          <div class="Ku" v-show="kuShowLast">
+             <router-link tag="a" to="/vuejs">Ку</router-link>
+          </div>
+      </transition>
+      
+      
+     
+    </div>    
   </div>
 </template>
 
 <script>
+  export default{
+    data(){
+      return{
+        kuShowTwo:false,
+        kuShowLast:false
+      }
+    }
+  }
 </script>
 
 <style lang="stylus" scoped>
