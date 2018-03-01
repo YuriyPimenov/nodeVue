284092a1dd6c8a9a1d018c141b47fa7ad0a9699d
YuriyPimenov <yuripimenov1991@gmail.com>
2018-03-01 16:14:39 +0500

    Debug eslint Форма аутентификации

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import path from 'path'
 import bodyParser from 'body-parser'
+import Debug from 'debug'
 
+const debug = Debug('server:app');
 const port = process.env.PORT || 5000
 const app = express()
 
@@ -10,12 +12,12 @@ app.use(bodyParser.json())
 app.use('/dist',express.static('dist'))
 
 //Для разработки, чтобы не перезапускать наш сервер
-import webpackDevConfig from './webpackDevConfig.js'
-webpackDevConfig(app);
+import devOptions from './controllers/config/devOptions.js'
+devOptions(app);
 
 app.get('/*', (req,res) => {
 	res.sendFile(path.join(__dirname, '../index.html'))
 })
 app.listen(port, () => {
-	console.log('Server listen on port = ',port,'ENV = ',process.env.NODE_ENV)
+	debug('Server listen on port = ',port,'ENV = ',process.env.NODE_ENV)
 })
---
+++ src/pages/auth.vue
@@ -0,0 +1,11 @@
+<template>
+	<auth-form></auth-form>
+</template>
+<script>
+import authForm from '../components/authForm.vue'
+	export default {
+		components:{
+			authForm
+		}
+	}
+</script>
