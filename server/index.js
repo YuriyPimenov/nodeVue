7e9efdeee19da473412df0e4a299c9300720bf61
YuriyPimenov <yuripimenov1991@gmail.com>
2018-03-02 23:24:29 +0500

    Создание action, route(server)

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import path from 'path'
 import bodyParser from 'body-parser'
 import Debug from 'debug'
 
+import auth from './routes/auth.js'
+
 const debug = Debug('server:app');
 const port = process.env.PORT || 5000
 const app = express()
@@ -14,6 +16,9 @@ app.use('/dist',express.static('dist'))
 //Для разработки, чтобы не перезапускать наш сервер
 import devOptions from './controllers/config/devOptions.js'
 devOptions(app);
+
+//обработка запросов - ROUTES
+app.use('/api/auth',auth)
 
 app.get('/*', (req,res) => {
 	res.sendFile(path.join(__dirname, '../index.html'))
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 //Состаяние
 const state = {
 	user:null
@@ -8,7 +9,22 @@ const mutations = {
 }
 //Делаем какое либо действие
 const actions = {
-
+	auth({commit},{email,password,repassword}){
+		console.log('email',email,'password',password,'repassword',repassword)
+		
+		if(password===repassword || !repassword){
+			axios.post('/api/auth',{
+				email,password
+			}).then((res)=>{
+				console.log('ответ = ',res.data)
+			}).catch((err)=>{
+				console.log('ошибка = ',err)
+			})
+		}else{
+			console.log('error waga')
+		}
+		
+	}
 }
 //Тут получаем состояние
 const getters = {
